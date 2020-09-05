a8e13759bd35f328eaa0d6ff1302a65ef77a9107
Tashfiq Rahman <tashfiq123@Tashfiqs-MacBook-Pro.local>
2020-09-04 20:45:15 -0700

    Prepare for deploy

---
+++ client/src/components/contacts/ContactForm.js
@@ -0,0 +1,107 @@
+import React, { useState, useContext, useEffect } from "react";
+import ContactContext from "../../context/contact/contactContext";
+
+const ContactForm = () => {
+  const contactcontext = useContext(ContactContext);
+  const { addContact, current, clearCurrent, updateContact } = contactcontext;
+
+  useEffect(() => {
+    if (current !== null) {
+      setContact(current);
+    } else {
+      setContact({
+        name: "",
+        email: "",
+        phone: "",
+        type: "personal",
+      });
+    }
+  }, [contactcontext, current]);
+
+  const [contact, setContact] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    type: "personal",
+  });
+
+  const { name, email, phone, type } = contact;
+  const onChange = (e) =>
+    setContact({ ...contact, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (current === null) {
+      addContact(contact);
+    } else {
+      updateContact(contact);
+    }
+    clearAll();
+  };
+
+  const clearAll = () => {
+    clearCurrent();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h2 className="text-primary">
+        {current ? "Edit Contact" : "Add Contact"}
+      </h2>
+      <input
+        type="text"
+        placeholder="name"
+        name="name"
+        value={name}
+        onChange={onChange}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        name="email"
+        value={email}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        placeholder="phone"
+        name="phone"
+        value={phone}
+        onChange={onChange}
+      />
+      <h5>Contact type</h5>
+      <input
+        type="radio"
+        name="type"
+        value="personal"
+        onChange={onChange}
+        checked={type === "personal"}
+      />
+      Persoanl{" "}
+      <input
+        type="radio"
+        name="type"
+        value="professional"
+        onChange={onChange}
+        checked={type === "professional"}
+      />
+      professional{" "}
+      <div>
+        <input
+          type="submit"
+          value={current ? "Edit Contact" : "Add Contact"}
+          className="btn btn-primary btn-block"
+        />
+      </div>
+      {current && (
+        <div>
+          <button className="btn btn-light btn-block" onClick={clearAll}>
+            Clear
+          </button>
+        </div>
+      )}
+    </form>
+  );
+};
+
+export default ContactForm;
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
+
+//conect database
+connectDB();
+
+//init middleware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the Contactkeeper API..." })
@@ -10,6 +18,16 @@ app.get("/", (req, res) =>
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+//serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
